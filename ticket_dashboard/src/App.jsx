176352fcdcff6a102300aa75b4db916a2cdcf9c5
nikhilsuresh77 <nikhilsuresh77@gmail.com>
2025-03-06 09:24:39 +0000

    chore: react app update

--- ticket_dashboard/src/App.jsx
+++ ticket_dashboard/src/App.jsx
@@ -1,13 +1,12 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
-import { FrappeProvider } from 'frappe-react-sdk'
 function App() {
   const [count, setCount] = useState(0)
 
   return (
 	<div className="App">
-	  <FrappeProvider>
+	  <>
 		<div>
 	  <div>
 		<a href="https://vitejs.dev" target="_blank">
@@ -23,14 +22,14 @@ function App() {
 		  count is {count}
 		</button>
 		<p>
-		  Edit <code>src/App.jsx</code> and save to test HMR
+		  Edit <code>src/App.jsx</code> 
 		</p>
 	  </div>
 	  <p className="read-the-docs">
 		Click on the Vite and React logos to learn more
 	  </p>
 	  </div>
-	  </FrappeProvider>
+	  </>
 	</div>
   )
 }
--- ticket_dashboard/src/main.jsx
+++ ticket_dashboard/src/main.jsx
@@ -1,10 +1,10 @@
-import { StrictMode } from 'react'
+import { FrappeProvider } from 'frappe-react-sdk'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
+  <FrappeProvider>
     <App />
-  </StrictMode>,
+  </FrappeProvider>,
 )
